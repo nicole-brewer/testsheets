d53b41296e77014cdecf6cd19622ac66f17608cf
enaY15 <elizabeth.aslinger@aya.yale.edu>
2020-05-26 19:29:37 -0400

    Bug fixes with writing rows and reading sheets (moved my code from earlier to just before write_testsheet_i in write_testsheet

--- R/write_testsheet.R
+++ R/write_testsheet.R
@@ -20,14 +20,20 @@ write_testsheet <- function(sheet_name, filename=NULL, filepath=NULL, overwrite=
   validate_sheet(sheet_name)
   connection <- open_file_connection(sheet_name, filename, filepath, overwrite)
   sheet <- read_testsheet_i(spreadsheet, sheet_name)
+
+  list_cols <- names(which(sapply(colnames(sheet), function(x) sum(grepl('c[(]', sheet[, x])) > 0)))
+  sheet <- data.frame(lapply(sheet[, list_cols], function(y) gsub("NA", "NULL", y)))
+  sheet <- data.frame(lapply(sheet[, list_cols], function(y) gsub('"', '', y)))
+
   write_testsheet_i(sheet, sheet_name, connection)
+
 }
 
 open_file_connection <- function(sheet_name, filename=NULL, filepath=NULL, overwrite=FALSE) {
 
   # default filename is the name of the sheet
   if (is.null(filename)) {
-    filename <- paste0('test_', sheet_name, '.R')
+    filename <- paste0('testsheet_', sheet_name, '.R')
   }
 
   # default path is cwd
@@ -57,39 +63,31 @@ write_testsheet_i <- function(sheet, sheet_name, connection) {
 }
 
 write_row <- function(row, sheet_name, connection) {
-
-  # remove NA's
   clean_row <- row %>% dplyr::select_if(~any(!is.na(.)))
-
-  # preprocess: separate parameters from expected output
-  params <- dplyr::select(clean_row, dplyr::starts_with("param_"))
-  params <- (dplyr::rename_at(params, dplyr::vars(dplyr::starts_with("param_")), ~ stringr::str_replace(., "param_", "")))
-  expects <- dplyr::select(clean_row, dplyr::starts_with("expect_"))
-
-  # test_that ...
+  params <- dplyr::select(clean_row, starts_with("param_"))
+  params <- (dplyr::rename_at(params, dplyr::vars(starts_with("param_")),
+                              ~stringr::str_replace(., "param_", "")))
+  expects <- dplyr::select(clean_row, starts_with("expect_"))
   writeLines(paste0("test_that( ", row$test_that, ", {"), connection)
-
-  # ...the function (as specified by the sheet title)...
   writeLines(paste0("\tactual <- ", sheet_name, "("), connection)
-
-  # ...with the given parameters...
   param_names <- colnames(params)
   len <- length(param_names)
   for (i in seq(len - 1)) {
     param_name <- param_names[i]
-    writeLines(paste0("\t\t", param_name, " = ", params[[param_name]], ","), connection)
+    writeLines(paste0("\t\t", param_name, " = ", params[[param_name]],
+                      ","), connection)
   }
-  # last parameter shouldn't be followed by a comma so we deal with it outside the for loop
   param_name <- param_names[len]
-  writeLines(paste0("\t\t", param_name, " = ", params[[param_name]]), connection)
-
+  writeLines(paste0("\t\t", param_name, " = ", params[[param_name]]),
+             connection)
   writeLines("\t)", connection)
   for (expect in colnames(expects)) {
-    # split at the second underscore. Example "expect_lte_power" -> "expect_lte" and "power"
     words <- unlist(strsplit(expect, "_"))
-    expect_function <- paste(words[1:2], collapse="_")
-    variable_name <- paste0("actual$",paste(words[3:length(words)], collapse="_"))
-    writeLines(paste0("\t", expect_function, "(", variable_name, ", ", expects[[expect]], ")"), connection)
+    expect_function <- paste(words[1:2], collapse = "_")
+    variable_name <- paste0("actual$", paste(words[3:length(words)],
+                                             collapse = "_"))
+    writeLines(paste0("\t", expect_function, "(", variable_name,
+                      ", ", expects[[expect]], ")"), connection)
   }
   writeLines("})\n", connection)
 }
